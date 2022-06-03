98aec2236db9cd128b935d167432dc217cfb9e0c
Macaulay5 <macaulaynmaram@gmail.com>
2022-06-02 17:52:00 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,25 +9,25 @@ if __name__ == "__main__":
 
     ops = {"+": add, "-": sub, "*": mul, "/": div}
 
-     try:
+    try:
         a, op, b = argv[1:]
 
-     except ValueError:
-          print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-           sys_exit(1)
+    except ValueError:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys_exit(1)
 
-     try:
-         print(a, op, b, "=", ops[op](int(a), int(b)))
+    try:
+        print(a, op, b, "=", ops[op](int(a), int(b)))
 
-     except KeyError:
-          print("Unknown operator. Available operators: +, -, * and /")
-           sys_exit(1)
+    except KeyError:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys_exit(1)
 
-     except ValueError:
-         print("Invalid operand. Must be an integer")
-         sys_exit(1)
+    except ValueError:
+        print("Invalid operand. Must be an integer")
+        sys_exit(1)
 
-     except ZeroDivisionError:
+    except ZeroDivisionError:
          print("Invalid operation. Cannot divide by zero")
          sys_exit(1)
 
